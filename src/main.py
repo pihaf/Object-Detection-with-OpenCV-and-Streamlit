6a3f79ab1f565d6eb8a8899f9569e3a16d87b605
pihaf <113176707+pihaf@users.noreply.github.com>
2024-06-27 17:24:47 +0700

    feat: Object detection and streamlit app

---
+++ src/main.py
@@ -0,0 +1,42 @@
+import os
+import cv2
+import numpy as np
+from PIL import Image
+import streamlit as st
+
+# Set model and prototxt paths
+MODEL = os.path.join(os.path.dirname(__file__), "model", "MobileNetSSD_deploy.caffemodel")
+PROTOTXT = os.path.join(os.path.dirname(__file__), "model", "MobileNetSSD_deploy.prototxt.txt")
+
+def process_image(image):
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+    )
+    net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
+    net.setInput(blob)
+    detections = net.forward()
+    return detections
+
+def annotate_image(image, detections, confidence_threshold=0.5):
+    (h, w) = image.shape[:2]
+    for i in np.arange(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > confidence_threshold:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (start_x, start_y, end_x, end_y) = box.astype("int")
+            cv2.rectangle(image, (start_x, start_y), (end_x, end_y), 70, 2)
+    return image
+
+def main():
+    st.title('Object Detection for Images')
+    file = st.file_uploader('Upload Image', type=['jpg', 'png', 'jpeg'])
+    if file is not None:
+        st.image(file, caption="Uploaded Image")
+        image = Image.open(file)
+        image = np.array(image)
+        detections = process_image(image)
+        processed_image = annotate_image(image, detections)
+        st.image(processed_image, caption="Processed Image")
+
+if __name__ == "__main__":
+    main()
---
+++ src/test/test_app.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+import cv2
+import numpy as np
+from main import process_image, annotate_image
+
+# Set paths for model files
+MODEL = os.path.join(os.path.dirname(__file__), "../model/MobileNetSSD_deploy.caffemodel")
+PROTOTXT = os.path.join(os.path.dirname(__file__), "../model/MobileNetSSD_deploy.prototxt.txt")
+
+def test_process_image():
+    # Load the actual image
+    image_path = os.path.join(os.path.dirname(__file__), '../public/dog.jpeg')
+    image = cv2.imread(image_path)
+    assert image is not None, f"Failed to load image from {image_path}"
+    
+    # # Update global variables if needed
+    # global MODEL, PROTOTXT
+    # MODEL = MODEL
+    # PROTOTXT = PROTOTXT
+    
+    detections = process_image(image)
+    assert detections is not None
+    assert detections.shape[2] > 0
+
+def test_annotate_image():
+    # Create a dummy image
+    dummy_image = np.zeros((300, 300, 3), dtype=np.uint8)
+    
+    # Create a dummy detection
+    dummy_detections = np.array([[[[0, 1, 0.9, 0.1, 0.1, 0.5, 0.5]]]])
+    
+    annotated_image = annotate_image(dummy_image, dummy_detections, confidence_threshold=0.5)
+    assert annotated_image is not None
+    assert annotated_image.shape == dummy_image.shape
